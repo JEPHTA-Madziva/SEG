6ef66806386cfe06071b6d53b175ee8f8517c004
JEPHTA <jephtaslab@gmail.com>
2025-09-04 20:35:48 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//---Dark & Light theme button setting--//
+var theme = document.getElementById("theme");
+
+theme.onclick = function()
+{
+    document.body.classList.toggle("dark-mode");
+}
+
+//---  ---//
